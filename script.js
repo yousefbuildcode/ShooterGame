10a8c6f5d2f4da5b2cc4d4ca3cf3e4398f93c8ca
yousefbuildcode <rashed2019alrwely@gmail.com>
2026-07-25 11:19:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -530,21 +530,4 @@ if(stageComplete){
 }
     requestAnimationFrame(gameLoop);
 
-}
-document.addEventListener("keydown",function(e){
-
-    if(stageComplete && e.key==="Enter"){
-
-        stage++;
-        targetKills+=10;
-
-        health=100;
-
-        enemies=[];
-
-        stageComplete=false;
-
-    }
-
-});
 gameLoop();
